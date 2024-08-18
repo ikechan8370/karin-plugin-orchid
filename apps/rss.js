8d9b90612325da046a0ae1fb2b9b5d3ea7c7c54e
ikechan8370 <geyinchibuaa@gmail.com>
2024-08-18 12:47:17 +0800

    fix: rss

--- apps/rss.js
+++ apps/rss.js
@@ -42,13 +42,17 @@ async function rss () {
         let ps = rssConfig.subscribe_list.map(async rss => {
             return new Promise(async (resolve, reject) => {
                 try {
-                    let url = rss.url
+                    let url = rss.url?.replaceAll('${rsshub_url}', rssConfig.rsshub_url)
                     let postHandlers = rss.postHandlers || []
                     if (rss.preset) {
                         const rssPreset = buildRssUrl(rss.presetConfig, rss.preset, rssConfig.rsshub_url)
-                        console.log(rssPreset)
+                        // console.log(rssPreset)
                         url = rssPreset.url
                         rssPreset.postHandlers && postHandlers.push(...rssPreset.postHandlers)
+                    }
+                    if (!url) {
+                        logger.warn(`rss ${rss.name} url not found`)
+                        resolve()
                     }
                     const news = await fetchRSS(rss.name, url, rss.rssParserConfig)
                     for (let res of news) {
@@ -69,6 +73,9 @@ async function rss () {
                                 }
                             }
                         }
+                        if (!newsItem) {
+                            continue
+                        }
                         let title = newsItem.title
                         let link = newsItem.link
                         let pubDate = newsItem.pubDate
@@ -79,16 +86,24 @@ async function rss () {
                         for (let karinAdapter of bot) {
                             let groups = rss.group || defaultGroups
                             for (let group of groups) {
+                                let background = rss.background || rssConfig.background || 'https://upload-bbs.miyoushe.com/upload/2024/08/08/11137146/a13030c06cea59159c6cab3d5538731d_6288383820731450582.jpg'
+                                if (!background.startsWith('http')) {
+                                    background = `${dirPath}/resources/image/${background}`
+                                }
                                 let img = await render.render({
                                     file: `./plugins/${PluginName}/resources/template/rss.html`,
                                     data: {
-                                        title: title,
+                                        title: `【${rss.name}】` + title,
                                         link: link,
                                         pubDate: formattedDate,
                                         pluResPath: `${dirPath}/resources/`,
                                         content: content,
-                                        name: rss.name
+                                        name: rss.name,
+                                        background
                                     },
+                                    pageGotoParams: {
+                                        waitUntil: 'networkidle2'
+                                    }
                                 })
                                 await karinAdapter.SendMessage(karin.contact(group, true), [segment.image(img)])
                             }
